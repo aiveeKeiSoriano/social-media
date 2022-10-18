e0f07b4e54a0589d9e9a909109c15b2568aa6e01
aiveeKeiSoriano <aivee.soriano7@gmail.com>
2022-10-19 04:04:48 +0800

    Add test endpoint

--- server/controllers/pingController.js
+++ server/controllers/pingController.js
@@ -1,7 +1,7 @@
 const Ping = require("../models/ping");
 
 const createPing = async (data) => {
-  let newPing = new Ping({name: data?.user?.email || 'Ping'});
+  let newPing = new Ping({name: data?.user?.user_id || 'Ping'});
   await newPing.save();
   return newPing;
 };
